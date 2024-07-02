ae2f69d89715b89be7e3f9ec08ed4ae25e4ee8e4
naoasa <asaumi1106n@gmail.com>
2024-07-03 01:05:06 +0900

    feat: 入力値をtxtファイルに追記する機能

--- password_manager.sh
+++ password_manager.sh
@@ -3,8 +3,19 @@
 echo "パスワードマネージャーへようこそ！"
 
 echo "サービス名を入力してください:"
+# サービス名の受け取り
+read service_name
+
 echo "ユーザー名を入力してください:"
+# ユーザー名の受け取り
+read user_name
+
 echo "パスワードを入力してください:"
+# パスワードの受け取り
+read password
+
+# 受け取り値を標準出力でファイルに追記
+echo "$service_name:$user_name:$password" 1>> password_storage.txt
 
 # 入力が完了したら
 echo "Thank you!"
