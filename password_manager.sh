85b10f082b2c2a974be3e9cfc998d59efa36dac7
naoasa <asaumi1106n@gmail.com>
2024-07-03 01:30:54 +0900

    feat: 文章の表示部分を追加

--- password_manager.sh
+++ password_manager.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo "パスワードマネージャーへようこそ！"
+echo "次の選択肢から入力してください(Add Password/Get Password/Exit):"
+
+
+# Add Password が入力された場合
 
 echo "サービス名を入力してください:"
 # サービス名の受け取り
@@ -18,4 +22,25 @@ read password
 echo "$service_name:$user_name:$password" 1>> password_storage.txt
 
 # 入力が完了したら
+echo "パスワードの追加は成功しました。"
+
+
+# Get Password が入力された場合
+
+echo "サービス名を入力してください:"
+
+## サービス名が保存されていなかった場合
+echo "そのサービスは登録されていません。"
+## サービス名が保存されていた場合
+echo "サービス名:hoge"
+echo "ユーザー名:fuga"
+echo "パスワード:piyo"
+
+
+# Exit が入力された場合
 echo "Thank you!"
+## プログラムが終了
+
+
+# Add Password/Get Password/Exit 以外が入力された場合
+echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
