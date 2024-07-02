778a8613f28230fe4bd6d1fcb89f960bcfea8f4e
naoasa <asaumi1106n@gmail.com>
2024-07-03 02:07:20 +0900

    feat: Get Passwordの実装中

--- password_manager.sh
+++ password_manager.sh
@@ -19,6 +19,23 @@ if [ "$choice" = "Add Password" ]; then
 	echo "$service_name:$user_name:$password" 1>> password_storage.txt
 	# 入力が完了したら
 	echo "パスワードの追加は成功しました。"
+
+# Get Password が入力された場合 => 入力値が"Get Password"と等しい
+elif [ "$choice" = "Get Password" ]; then
+	echo "サービス名を入力してください:"
+	# 入力されたサービス名を変数に格納
+	read get_service_name
+
+	## サービス名が保存されていた場合
+	if [ "$get_service_name" = "yahoo" ]; then
+		echo "サービス名:hoge"
+		echo "ユーザー名:fuga"
+		echo "パスワード:piyo"
+	## サービス名が保存されていなかった場合
+	else
+	## get_service_nameと保管庫txtから「"先頭〜":までの文字列を検索したものが不一致の場合
+		echo "そのサービスは登録されていません。"
+	fi
 else
 	# Add Password/Get Password/Exit 以外が入力された場合
 	echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
@@ -26,16 +43,6 @@ fi
 
 
 
-# Get Password が入力された場合
-
-echo "サービス名を入力してください:"
-
-## サービス名が保存されていなかった場合
-echo "そのサービスは登録されていません。"
-## サービス名が保存されていた場合
-echo "サービス名:hoge"
-echo "ユーザー名:fuga"
-echo "パスワード:piyo"
 
 
 # Exit が入力された場合
